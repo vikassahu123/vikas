2960d00434f2a7aba5613fe8c3028b5f93638fc9
Vikassahu62 <vikassahu54927@gamil.com>
2025-01-07 14:01:36 +0530

    this is master branch

---
+++ Calci.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public static Calci{
+
+ public static void main(String args[]){
+System.out println("this is containing main method");
+int a,b;
+a=sc.nextInt();
+b=sc.nextInt();
+
+System.out println("this will result addition = "+ add(a,b));
+
+System.out println("this will result multiplication = "+ mul(a,b));
+System.out println("this will result subtraction = "+ sub(a,b));
+System.out println("this will result division = "+ div(a,b));
+
+
+}
+
+}
